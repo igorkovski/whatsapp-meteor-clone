d6d8f6788f6cb419784d339b3d2564dec5b9c10a
igor <igor@Mac-Pro.local>
2015-10-17 12:52:39 -0400

    create server bootstrap

--- client/scripts/routes.ng.js
+++ client/scripts/routes.ng.js
@@ -1,22 +1,23 @@
 angular
-	.module('Whatsapp')
-	.config(config);
+  .module('Whatsapp')
+  .config(config);
 
 function config($stateProvider, $urlRouterProvider) {
-	$stateProvider
-		.state('tab', {
-			url: '/tab',
-			abstract: true,
-			templateUrl: 'client/templates/tabs.ng.html'
-	})
-	.state('tab.chats', {
-		url: '/chats',
-		views: { 
-			'tab-chats': {
-		templateUrl: 'client/tamplates/chats.ng.html',
-		controller: 'ChatsCtrl'
-		}
-	}
-	});
-	$urlRouterProvider.otherwise('tab/chats');
+  $stateProvider
+    .state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'client/templates/tabs.ng.html'
+    })
+    .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'client/templates/chats.ng.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('tab/chats');
 }
