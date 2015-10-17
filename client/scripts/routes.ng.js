f5b72d2fdbf5da1d4ba4b44fcda5e13fecc3fd9f
igor <igor@Mac-Pro.local>
2015-10-17 11:47:06 -0400

    initial commit

---
+++ client/scripts/routes.ng.js
@@ -0,0 +1,19 @@
+angular.module('Whatsapp').config(config);
+
+function config($stateProvider, $urlRouterProvider) {
+	$stateProvider
+	.state('tab', {
+		url: '/tab',
+		abstract: true,
+		templateUrl: 'client/templates/tabs.ng.html'
+	})
+	.state('tab.chats', {
+		url: '/chats',
+		views: 
+		{ 'tab-chats': {
+		templateUrl: '/client/tamplates/chats.ng.html'
+		}
+	}
+	});
+	$urlRouterProvider.otherwise('tab/chats');
+}
